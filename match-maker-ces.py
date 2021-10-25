632c44fe71d42c62b29916d1a0f40135ee0ef386
carlyshipman <89702823+carlyshipman@users.noreply.github.com>
2021-10-24 22:02:26 -0500

    Update match-maker-ces.py

--- match-maker-ces.py
+++ match-maker-ces.py
@@ -1,5 +1,23 @@
 #Carly Shipman
-#Matchmaker Lite - Part 2
+#Matchmaker
+
+
+QUESTION = [
+    'Ironman is the best MCU character',
+    'Calculus is dumb',
+    'Running is really fun',
+    'Matcha tastes like grass',
+    'Hippo Campus is the best band ever'
+]
+
+
+DESIRED_RESPONSE =[
+    5, #strongly agree
+    1, #strongly disagree
+    5, #srongly agree
+    2, #agree
+    5, #strongly agree
+]
 
 INTRODUCTION = '''
 Matchmaker 2021
@@ -15,6 +33,48 @@ receive a compability score. The higher,
 the more compabitable we are. Good luck!
 
 '''
+MAX_SCORE = 5 *len(QUESTION)
+response = []
+compatability = []
+
+print(INTRODUCTION)
+
+#ask all questions
+
+for i in range(len(QUESTION)):
+	def requestaValidNumberBetween1and5():
+		UserResponse2String= str(input(QUESTION[i]))
+		print("you entered: "+ UserResponse2String)
+
+		if not UserResponse2String.isnumeric():
+			print("This is not a number!")
+			return False
+		else:
+			UserResponse2Int = int(UserResponse2String)
+			if (UserResponse2Int < 1) or (UserResponse2Int > 5):
+				print("\n This is not a number between 1 and 5 >:(")
+				return False
+			else:
+				return True
+	
+	userHasEnteredANumberBetween1and5 = False
+	while not userHasEnteredANumberBetween1and5:
+		userHasEnteredANumberBetween1and5 = requestaValidNumberBetween1and5()
+		print(userHasEnteredANumberBetween1and5)
+		if not (userHasEnteredANumberBetween1and5):
+			print("please try again.")
+	
+	response.append(UserResponse2String)
+	questionCompatibility = 5 - abs(userResponse2String - DESIRED_RESPONSE[i])
+    	compatability.append(questionCompatibility)
+
+	# String formatting with parameters and placeholders.
+    	print('Question %d compatability: %d' % (i+1, questionCompatibility))
+
+	
+#Carly Shipman
+#Matchmaker
+
 
 QUESTION = [
     'Ironman is the best MCU character',
@@ -24,6 +84,7 @@ QUESTION = [
     'Hippo Campus is the best band ever'
 ]
 
+
 DESIRED_RESPONSE =[
     5, #strongly agree
     1, #strongly disagree
@@ -32,35 +93,56 @@ DESIRED_RESPONSE =[
     5, #strongly agree
 ]
 
-MAX_SCORE = 5 * len(QUESTION)
-
-print(INTRODUCTION)
-
-# Ask alll questions
+INTRODUCTION = '''
+Matchmaker 2021
 
 
+Hello, here you can learn if you and I
+are a perfect match! To see, anwser the
+following questions with an number 1-5.
+1 meaning completely disagree, 3 means
+in between,and 5 meaning completely agree.
+after completing each question, you will
+receive a compability score. The higher,
+the more compabitable we are. Good luck!
+
+'''
 response = []
 compatability = []
 
-# Ask all questions.
-for i in range(len(QUESTION)):
-    userResponse = int(input(QUESTION[i]))
-    #Todo: Validate response and ask quesion again if necessary
+print(INTRODUCTION)
 
-    response.append(userResponse)
+#ask all questions
 
-    questionCompatibility = 5 - abs(userResponse - DESIRED_RESPONSE[i])
+
+def requestaValidNumberBetween1and5():
+    for i in range(len(QUESTION)):
+		UserResponse2String= str(input(QUESTION[i]))
+		    print("you entered: "+ UserResponse2String)
+
+		if not UserResponse2String.isnumeric():
+			print("This is not a number!")
+			return False
+		else:
+			UserResponse2Int = int(UserResponse2String)
+			if (UserResponse2Int < 1) or (UserResponse2Int > 5):
+				print("\n This is not a number between 1 and 5 >:(")
+				return False
+			else:
+				return True
+	
+	userHasNotEnteredANumberBetween1and5 = False
+	while not userHasNotEnteredANumberBetween1and5:
+		userHasNotEnteredANumberBetween1and5 = requestaValidNumberBetween1and5()
+		print(userHasNotEnteredANumberBetween1and5)
+		if not (userHasNotEnteredANumberBetween1and5):
+			print("please try again.")
+	
+	response.append(UserResponse2String)
+	questionCompatibility = 5 - abs(userResponse2String - DESIRED_RESPONSE[i])
     compatability.append(questionCompatibility)
 
-    # String formatting with parameters and placeholders.
+	# String formatting with parameters and placeholders.
     print('Question %d compatability: %d' % (i+1, questionCompatibility))
 
-
-totalCompatability = 0
-for c in compatability:
-    totalCompatability += c
-
-totalCompatability *= 100 / MAX_SCORE
-print('Total Compatability: %d\n\n' % (totalCompatability))
-
-#Todo: Determine compatability ranges
+	
